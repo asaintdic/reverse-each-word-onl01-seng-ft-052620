873e3b97b25f07b0ddd329d94f830d1ed5be2b2a
Alain Saint-Dic <alains214@gmail.com>
2020-06-01 16:58:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,5 @@ new_array.join(" ")
 end
 reverse_each_word("Hello there, and how are you?")
 
+#iterate over each word in array ^^^
+# return those words back to a new arrayconvert that array back to a string
